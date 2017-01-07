ba9b4c707fe99d500ea72197d39dd07f9de3cc1f
puz_ya <puzino.y.a@gmail.com>
2017-01-07 18:08:53 +0300

    Added second fragment layout; MainActivity now listen to clicks on ListFragment; WorkOutListFragment gets names + details, includes interface - listener, listen onListItemClick

--- app/src/main/java/com/yd/workoutnow/MainActivity.java
+++ app/src/main/java/com/yd/workoutnow/MainActivity.java
@@ -4,14 +4,20 @@ import android.app.Activity;
 //import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class MainActivity extends Activity {
+public class MainActivity
+        extends Activity
+        implements WorkOutListFragment.WorkOutListener
+{
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        WorkOutDetailFragment workOutDetailFragment = (WorkOutDetailFragment)getFragmentManager().findFragmentById(R.id.detail_fragment);
-        workOutDetailFragment.setWorkout(1);
+    }
+
+    @Override
+    public void itemClicked(long id){
+        //details for future
     }
 }
